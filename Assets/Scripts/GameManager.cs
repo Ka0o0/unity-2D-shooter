25eab008f99439aad0dd77305cfe4214fbd2a7fd
Kai Takac <kai.takac@gmail.com>
2017-11-22 17:40:10 +0100

    ADD Ryder project and GameManager 💃

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject PlayerPrefab;
+
+
+    private void Start()
+    {
+        Instantiate(PlayerPrefab);
+    }
+}
