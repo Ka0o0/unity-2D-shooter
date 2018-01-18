48a88a027cd09082d0ab43b7acfceb3d9ec2d1af
Christoph Muck <muckz11@gmail.com>
2018-01-18 12:02:46 +0100

    Bullet Distance restriction 🏹

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,16 +1,30 @@
-﻿using UnityEngine;
+﻿using NUnit.Framework.Constraints;
+using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     public double Damage;
     public int Speed;
-    
+    public float MaxTravelDistance;
+
+    private Vector3 _startPosition;
+
     public void StartMove()
     {
-        transform.position = transform.position + transform.up;
-        
+        _startPosition = transform.position;
+        transform.position = _startPosition + transform.up;
+
         var bulletRigidbody2D = GetComponent<Rigidbody2D>();
         bulletRigidbody2D.AddForce(transform.up * 500);
+    }
+
+    private void FixedUpdate()
+    {
+        var travelledDistance = Vector3.Distance(transform.position, _startPosition);
+        if (travelledDistance > MaxTravelDistance)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
