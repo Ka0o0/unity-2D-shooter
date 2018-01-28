6886c73e6552d2d4a88047dc81117cdc91ad5d51
Kai Takac <kai.takac@gmail.com>
2018-01-28 10:58:11 +0100

    ADD game restart action

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-
     public List<GameObject> Soldiers;
-    
-    
+
+    private List<string> AvailableScenes = new List<string>(new[] {"Level00"});
+    private Random _random = new Random();
+
+    public void RestartGame()
+    {
+        var randomIndex = Random.Range(0, AvailableScenes.Count);
+        var selectedScene = AvailableScenes[randomIndex];
+        SceneManager.LoadScene(selectedScene);
+    }
 }
